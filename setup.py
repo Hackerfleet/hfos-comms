e70e941c03abf9718a6fbf6fb2ad1fb72be3c20c
ri0t <riot@c-base.org>
2018-12-06 21:43:44 +0100

    Final content split HFOS -> Isomer

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ Communication device assistance.
 This software package is a plugin module for HFOS.
 """,
       dependency_links=[],
-      install_requires=['hfos>=1.2.0'],
+      install_requires=['isomer>=1.0.0'],
       entry_points="""[isomer.schemata]
     radioconfig=hfos.comms.radio:RadioConfig
     [isomer.components]
